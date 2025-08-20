b26d88d1cf402dd87496cc22c974305c57055d20
Sujal Naik <137867538+bsujalnaik@users.noreply.github.com>
2025-08-20 18:22:11 +0530

    app.py

--- app.py
+++ app.py
@@ -113,5 +113,5 @@ def analyze():
         except Exception as e:
             return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0')
